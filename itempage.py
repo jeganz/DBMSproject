621aac418acd4b268ce85fac27f1ed325a8d4c6d
AxelStrange <alexgeorge.tharayil@gmail.com>
2023-06-04 17:15:13 +0530

    change title bar

--- itempage.py
+++ itempage.py
@@ -8,6 +8,18 @@ import mysql.connector
 
 class itempage:
     def __init__(self,itemframe,create_connection):
+        
+        def dark_title_bar(window):
+            window.update()
+            DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+            set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
+            get_parent = ct.windll.user32.GetParent
+            hwnd = get_parent(window.winfo_id())
+            rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
+            value = 2
+            value = ct.c_int(value)
+            set_window_attribute(hwnd, rendering_policy, ct.byref(value),
+                                ct.sizeof(value))
 
         heading=customtkinter.CTkLabel(itemframe,text='Item Details',font=('Century Gothic', 18,'bold','underline'),width=100,text_color='black')
         heading.place(x=14,y=10)
